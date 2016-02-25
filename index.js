48b1847023f77f43dca369bf829855d8d436bcb9
Geoffrey Goodman <ggoodman@gmail.com>
2016-02-25 13:11:30 -0500

    Implement text/event-stream parser. Fixes auth0/wt-cli#49

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var JSONStream = require('json-stream').JSONStream;
 var Request = require('request-stream');
+var Stream = require('stream');
 var Util = require('util');
 
 
@@ -9,8 +9,19 @@ module.exports = createStream;
 function createStream(url, options) {
     if (!options) options = {};
     
+    
+    
     var stream = new LogStream(options);
-    var req = Request.get(url, onResponse);
+    var req = Request.get(url, {
+        headers: {
+            'Accept': 'text/event-stream',
+        },
+    },onResponse);
+    
+    req.once('close', function () {
+        stream.emit('error', new Error('Connection closed'));
+        stream.destroy();
+    });
     
     req.once('socket', function () {
         stream.emit('open');
@@ -33,13 +44,70 @@ function createStream(url, options) {
 function LogStream(options) {
     if (!options) options = {};
     
-    JSONStream.call(this, options);
+    this._buffer;
+    this._event;
+    
+    Stream.Transform.call(this, { readableObjectMode: true });
 }
 
-Util.inherits(LogStream, JSONStream);
+Util.inherits(LogStream, Stream.Transform);
+
+LogStream.prototype._flush = function (cb) {
+    if (this._event) this.push(this._event);
+    
+    return cb();
+};
+
+LogStream.prototype._transform = function(chunk, encoding, cb) {
+    if (!Buffer.isBuffer(chunk)) {
+        chunk = new Buffer(chunk);
+    }
+    if (this._buffer) {
+        chunk = Buffer.concat([this._buffer, chunk]);
+    }
+
+    var ptr = 0;
+    var start = 0;
+    
+    for (ptr = 0; ptr < chunk.length; ptr++) {
+        if (chunk[ptr] === 10) {
+            var line = chunk.slice(start, ptr).toString('utf8');
+            var matches = line.match(/^([^:]*):(.*)$/);
+            
+            if (matches) {
+                if (!this._event) {
+                    this._event = { event: 'data', };
+                }
+                
+                if (matches[1]) {
+                    this._event[matches[1]] = matches[2];
+                }
+            } else {
+                console.error('Line failed to match', line);
+            }
+            
+            if (ptr < chunk.length - 1 && chunk[ptr + 1] === 10) {
+                if (this._event.event === 'data') {
+                    try {
+                        this.push(JSON.parse(this._event.data));
+                    } catch (__) { }
+                }
+                
+                this._event = null;
+                
+                ptr++;
+            }
+            
+            start = ptr + 1;
+        }
+    }
+    
+    this._buffer = chunk.slice(start);
+    
+    return cb();
+};
 
 LogStream.prototype.destroy = function () {
     this.emit('close');
     this.unpipe();
-    this.abort();
 };
