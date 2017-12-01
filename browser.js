8b747149a8dd6dea984d7b7f8f6710dcfd5b3744
Geoffrey Goodman <ggoodman@gmail.com>
2017-12-01 15:01:36 -0500

    Refactor browser implementation

--- browser.js
+++ browser.js
@@ -1,93 +1,110 @@
+/* eslint-env browser */
+
 'use strict';
 
 var Stream = require('stream');
 
+class WebtaskLogStream extends Stream.Readable {
+    constructor(url, opts) {
+        if (!opts) opts = {};
+
+        super({ objectMode: true });
+
+        this._destroyed = false;
+        this._eventSource = new EventSource(url);
+        this._opened = false;
+        this._timeout = opts.timeout;
+        this._timeoutTimer = null;
+
+        this._eventSource.onerror = this._onError.bind(this);
+        this._eventSource.onmessage = this._onMessage.bind(this);
+        this._eventSource.onopen = this._onOpen.bind(this);
+        this._eventSource.addEventListener('ping', this._onPing.bind(this));
+
+        this._resetTimeout();
+    }
+
+    destroy() {
+        this._destroyed = true;
+        this._eventSource.close();
+
+        if (this._timeoutTimer) clearTimeout(this._timeoutTimer);
+
+        this.emit('close');
+    }
+
+    _onError() {
+        this._destroyed = true;
+        this._eventSource.close();
+
+        if (this._timeoutTimer) clearTimeout(this._timeoutTimer);
+
+        if (this.listeners('error').length) {
+            const error =
+                this._eventSource.readyState === 0
+                    ? new Error('Connection lost')
+                    : new Error('Connection error');
+            this.emit('error', error);
+        }
+    }
+
+    _onMessage(e) {
+        this._resetTimeout();
+
+        const msg = decode(e.data);
+        if (typeof msg !== 'undefined') this.push(msg);
+    }
+
+    _onOpen() {
+        this._resetTimeout();
+        this.emit('open');
+    }
+
+    _onPing() {
+        this._resetTimeout();
+    }
+
+    _onTimeout() {
+        this._destroyed = true;
+        this._eventSource.close();
+
+        if (this.listeners('error').length) {
+            const msg = this._opened
+                ? 'Connectivity timed out after ' +
+                  this._timeout +
+                  'ms inactivity'
+                : 'Connection timed out after ' + this._timeout + 'ms';
+            const error = new Error(msg);
+            error.code = 'E_TIMEDOUT';
+
+            this.emit('error', error);
+        }
+    }
+
+    _resetTimeout() {
+        if (!this._timeout) {
+            return;
+        }
+
+        if (this._timeoutTimer) {
+            clearTimeout(this._timeoutTimer);
+        }
+
+        this._timeoutTimer = setTimeout(
+            this._onTimeout.bind(this),
+            this._timeout
+        );
+    }
+}
 
 module.exports = function createStream(url, opts) {
-    if (!opts) opts = {};
-
-    var destroyed = false;
-    var rs = new Stream.Readable({
-        objectMode: true
-    });
-
-    rs._read = function() {};
-
-    rs.destroy = function() {
-        if (destroyed) return;
-        destroyed = true;
-        rs.emit('close');
-    };
-
-    connect(url, rs, opts);
-
-    return rs;
-}
-
-function connect(url, rs, options) {
-    var timeout;
-    var es = new window.EventSource(url);
-
-    rs.once('close', onClose);
-
-    es.onopen = function() {
-        rs.emit('open');
-
-        resetTimeout();
-    };
-
-    es.addEventListener('ping', resetTimeout);
-
-    es.onmessage = function(e) {
-        rs.push(decode(e.data));
-
-        resetTimeout();
-    };
-
-    es.onerror = function() {
-        var error = es.readyState === 0
-            ?   new Error('Connection lost')
-            :   new Error('Connection error');
-
-        if (rs.listeners('error').length) rs.emit('error', error);
-    };
-
-    function onClose() {
-        if (timeout) {
-            clearTimeout(timeout);
-            timeout = null;
-        }
-
-        if (!es.readyState !== 2) {
-            es.close();
-        }
-    }
-
-    function onTimeout() {
-        var error = new Error('Connection timed out');
-
-        error.code = 'E_TIMEDOUT';
-
-        rs.emit('error', error);
-
-        onClose();
-    }
-
-    function resetTimeout() {
-        if (timeout) {
-            clearTimeout(timeout);
-        }
-
-        // Default to 20s timeout
-        timeout = setTimeout(onTimeout, options.timeout || 20 * 1000);
-    }
-}
+    return new WebtaskLogStream(url, opts);
+};
 
 function decode(data) {
     try {
         return JSON.parse(data);
-    }
-    catch (err) {
+    } catch (err) {
         return undefined;
     }
 }
